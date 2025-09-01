91d504942e7ddfaccdacd4702b1324a683d39c56
Tkachez <tkachez@gmail.com>
2025-09-01 17:12:26 +0200

    initial

--- backend/src/routes/test.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-const router: Router = Router();
-
-router.get('/', (req, res) => console.log(111))
-
-export default router;
--- backend/src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-import config from './config/index';
-
-app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-});
