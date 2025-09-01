98fba06c1ef2127e40d9dff18e71c729d5ea4240
Tkachez <tkachez@gmail.com>
2025-09-01 15:52:05 +0200

    configured prettier

--- frontend/src/components/GameMenu/GameMenuComponent.tsx
+++ frontend/src/components/GameMenu/GameMenuComponent.tsx
@@ -1,8 +1,15 @@
 import styles from './GameMenu.module.css'
+import React from 'react'
 
-const GameMenuComponent = () => (
+type GameMenuComponentProps = {
+  onApprove: () => void
+  onCancel: () => void
+}
+
+const GameMenuComponent: React.FC<GameMenuComponentProps> = ({ onApprove, onCancel }) => (
   <div className={styles.menu__container}>
-    <button></button>
+    <button onClick={onApprove}>Approve</button>
+    <button onClick={onCancel}>Cancel</button>
   </div>
 )
 
--- frontend/src/components/GameMenu/GameMenuContainer.tsx
+++ frontend/src/components/GameMenu/GameMenuContainer.tsx
@@ -1,7 +1,15 @@
 import GameMenuComponent from './GameMenuComponent.tsx'
 
 const GameMenuContainer = () => {
-  return <GameMenuComponent />
+  const handleCancel = () => {
+    console.log('cancel')
+  }
+
+  const handleApprove = () => {
+    console.log('approve')
+  }
+
+  return <GameMenuComponent onApprove={handleApprove} onCancel={handleCancel} />
 }
 
 export default GameMenuContainer
