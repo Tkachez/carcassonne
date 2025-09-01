4bc51d5c790299088762a33cd59100357fe7b4f0
Tkachez <tkachez@gmail.com>
2025-09-01 16:25:06 +0200

    added lint in pre-commit

--- frontend/src/components/GameMenu/GameMenuComponent.tsx
+++ frontend/src/components/GameMenu/GameMenuComponent.tsx
@@ -2,15 +2,17 @@ import styles from './GameMenu.module.css'
 import React from 'react'
 
 type GameMenuComponentProps = {
-  onApprove: () => void
-  onCancel: () => void
+    onApprove: () => void
+    onCancel: () => void
 }
 
-const GameMenuComponent: React.FC<GameMenuComponentProps> = ({ onApprove, onCancel }) => (
-  <div className={styles.menu__container}>
-    <button onClick={onApprove}>Approve</button>
-    <button onClick={onCancel}>Cancel</button>
-  </div>
-)
+const GameMenuComponent: React.FC<GameMenuComponentProps> = ({onApprove, onCancel}) => {
+    return (
+        <div className={styles.menu__container}>
+            <button onClick={onApprove}>Approve</button>
+            <button onClick={onCancel}>Cancel</button>
+        </div>
+    )
+}
 
 export default GameMenuComponent
